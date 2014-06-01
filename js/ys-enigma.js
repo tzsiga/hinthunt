39911093cf1f184df340ccfc76c4caacde998cf1
tzsiga <tzsiga@gmail.com>
2014-06-01 16:30:17 +0200

    initial commit, poc version of enigma, gauges, digital display

---
+++ js/ys-enigma.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+  var codeTable = new Array();
+  /*
+  codeTable[32] = "Space";
+  codeTable[37] = "Left";
+  codeTable[38] = "Up";
+  codeTable[39] = "Right";
+  codeTable[40] = "Down";
+  codeTable[48] = "0";
+  codeTable[49] = "1";
+  codeTable[50] = "2";
+  codeTable[51] = "3";
+  codeTable[52] = "4";
+  codeTable[53] = "5";
+  codeTable[54] = "6";
+  codeTable[55] = "7";
+  codeTable[56] = "8";
+  codeTable[57] = "9";
+  */
+  codeTable[65] = "A";
+  codeTable[66] = "B";
+  codeTable[67] = "C";
+  codeTable[68] = "D";
+  codeTable[69] = "E";
+  codeTable[70] = "F";
+  codeTable[71] = "G";
+  codeTable[72] = "H";
+  codeTable[73] = "I";
+  codeTable[74] = "J";
+  codeTable[75] = "K";
+  codeTable[76] = "L";
+  codeTable[77] = "M";
+  codeTable[78] = "N";
+  codeTable[79] = "O";
+  codeTable[80] = "P";
+  codeTable[81] = "Q";
+  codeTable[82] = "R";
+  codeTable[83] = "S";
+  codeTable[84] = "T";
+  codeTable[85] = "U";
+  codeTable[86] = "V";
+  codeTable[87] = "W";
+  codeTable[88] = "X";
+  codeTable[89] = "Y";
+  codeTable[90] = "Z";
+
+  var keyesPressed = new Array();
+
+  $("html").keydown(function(e) {
+    var pressed = codeTable[e.keyCode];
+
+    if (pressed !== undefined) {
+      e.preventDefault();
+      $("#_" + pressed).css("color", "white");
+      $("#_" + pressed).css("background-color", "red");
+    }
+  });
+
+  $("html").keyup(function(e) {
+    var pressed = codeTable[e.keyCode];
+    if (pressed !== undefined) {
+      $("#_" + pressed).css("color", "auto");
+      $("#_" + pressed).css("background-color", "auto");
+
+      keyesPressed.push(e.keyCode);
+      console.log(keyesPressed);
+    }
+  });
+});
