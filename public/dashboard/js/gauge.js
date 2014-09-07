eedea5cbf6bfb1625a144fc147021609f0c715d2
tzsiga <tzsiga@gmail.com>
2014-09-07 11:35:23 +0200

    dashboard init form localStorage

--- public/dashboard/js/gauge.js
+++ public/dashboard/js/gauge.js
@@ -1,41 +1,46 @@
 $(document).ready(function () {
+  var initValue = localStorage.gaugeValue !== undefined ? localStorage.gaugeValue : 0;
 
-  var gauge = AmCharts.makeChart("gauge", {
-    "type": "gauge",
-    "theme": "none",
-    "fontSize": 28,
-    "color": "white",
-    "axes": [
+  var gauge = AmCharts.makeChart('gauge', {
+    'type': 'gauge',
+    'theme': 'none',
+    'fontSize': 28,
+    'color': 'white',
+    'axes': [
       {
-        "axisThickness": 20,
-        "axisColor": "#FFFFFF",
-        "axisAlpha": 1,
-        "tickThickness": 2,
-        "tickAlpha": 1,
-        "valueInterval": 20,
-        "bands": [
+        'axisThickness': 20,
+        'axisColor': '#FFFFFF',
+        'axisAlpha': 1,
+        'tickThickness': 2,
+        'tickAlpha': 1,
+        'valueInterval': 20,
+        'bands': [
           {
-            "color": "#AAAAAA",
-            "endValue": 260,
-            "startValue": 0
+            'color': '#AAAAAA',
+            'endValue': 260,
+            'startValue': 0
           }
         ],
-        "endValue": 260
+        'endValue': 260
       }
     ],
-    "arrows": [
+    'arrows': [
       {
-        "nailAlpha": 1,
-        "nailRadius": 10,
-        "startWidth": 12,
-        "color": "white"
+        'nailAlpha': 1,
+        'nailRadius': 10,
+        'startWidth': 12,
+        'color': 'white',
+        'value': initValue
       }
     ]
   });
 
+  $('#gauge-value').text(initValue);
+
   var socket = io();
 
   socket.on('setPressure', function (value) {
+    localStorage.gaugeValue = value;
     setGaugeValue(value);
   });
 
@@ -43,5 +48,4 @@ $(document).ready(function () {
     gauge.arrows[0].setValue(Math.round(value));
     $('#gauge-value').text(value);
   }
-
 });
--- public/dashboard/js/onload.js
+++ public/dashboard/js/onload.js
@@ -1,5 +1,54 @@
 ﻿$(document).ready(function () {
+  preloadImages();
+  initialize();
 
+  var socket = io();
+
+  socket.on('connect', function () {
+    socket.emit('CustomId', 'dashboard');
+  });
+
+  socket.on('setCrossIcon', function (value) {
+    localStorage.crossIcon = value;
+    setCrossIcon(value);
+  });
+
+  socket.on('setGaugeIcon', function (value) {
+    localStorage.gaugeIcon = value;
+    setGaugeIcon(value);
+  });
+
+  socket.on('setKeyIcon', function (value) {
+    localStorage.keyIcon = value;
+    setKeyIcon(value);
+  });
+
+  socket.on('setTorpedoIcon', function (value) {
+    localStorage.torpedoIcon = value;
+    setTorpedoIcon(value);
+  });
+
+  socket.on('setDeg', function (value) {
+    localStorage.deg = value;
+    setDeg(value);
+  });
+
+  socket.on('setLong', function (value) {
+    localStorage.long = value;
+    setLong(value);
+  });
+});
+
+function initialize() {
+  localStorage.crossIcon !== undefined ? setCrossIcon(localStorage.crossIcon) : setCrossIcon('red');
+  localStorage.gaugeIcon !== undefined ? setGaugeIcon(localStorage.gaugeIcon) : setGaugeIcon('red');
+  localStorage.keyIcon !== undefined ? setKeyIcon(localStorage.keyIcon) : setKeyIcon('red');
+  localStorage.torpedoIcon !== undefined ? setTorpedoIcon(localStorage.torpedoIcon) : setTorpedoIcon('red');
+  localStorage.deg !== undefined ? setDeg(localStorage.deg) : setDeg(0);
+  localStorage.long !== undefined ? setLong(localStorage.long) : setLong(0);
+}
+
+function preloadImages() {
   $.fn.preload = function () {
     this.each(function () {
       $('<img/>')[0].src = this;
@@ -12,67 +61,67 @@
     '../assets/image/icons/icon-key-green.png',
     '../assets/image/icons/icon-torpedo-green.png'
   ]).preload();
+}
 
-  var socket = io();
+function resetLocalStorage() {
+  for (var key in localStorage) {
+    if (localStorage.hasOwnProperty(key))
+      delete localStorage[key];
+  }
+}
 
-  socket.on('connect', function () {
-    socket.emit('CustomId', 'dashboard');
-  });
+function setCrossIcon(value) {
+  $('#icon-cross').html(
+    $('<img>', {
+      src: (value == 'red') ?
+        '../assets/image/icons/icon-cross-red.png' :
+        '../assets/image/icons/icon-cross-green.png',
+      style: 'width: 200px',
+      alt: ''
+    })
+  );
+}
 
-  socket.on('setCrossIcon', function (value) {
-    $('#icon-cross').html(
-      $('<img>', {
-        src: (value == 'red') ?
-          '../assets/image/icons/icon-cross-red.png' :
-          '../assets/image/icons/icon-cross-green.png',
-        style: 'width: 200px',
-        alt: ''
-      })
-    );
-  });
+function setGaugeIcon(value) {
+  $('#icon-gauge').html(
+    $('<img>', {
+      src: (value == 'red') ?
+        '../assets/image/icons/icon-gauge-red.png' :
+        '../assets/image/icons/icon-gauge-green.png',
+      style: 'width: 180px; margin-left: 30px',
+      alt: ''
+    })
+  );
+}
 
-  socket.on('setGaugeIcon', function (value) {
-    $('#icon-gauge').html(
-      $('<img>', {
-        src: (value == 'red') ?
-          '../assets/image/icons/icon-gauge-red.png' :
-          '../assets/image/icons/icon-gauge-green.png',
-        style: 'width: 180px; margin-left: 30px',
-        alt: ''
-      })
-    );
-  });
+function setKeyIcon(value) {
+  $('#icon-key').html(
+    $('<img>', {
+      src: (value == 'red') ?
+        '../assets/image/icons/icon-key-red.png' :
+        '../assets/image/icons/icon-key-green.png',
+      style: 'width: 200px',
+      alt: ''
+    })
+  );
+}
 
-  socket.on('setKeyIcon', function (value) {
-    $('#icon-key').html(
-      $('<img>', {
-        src: (value == 'red') ?
-          '../assets/image/icons/icon-key-red.png' :
-          '../assets/image/icons/icon-key-green.png',
-        style: 'width: 200px',
-        alt: ''
-      })
-    );
-  });
+function setTorpedoIcon(value) {
+  $('#icon-torpedo').html(
+    $('<img>', {
+      src: (value == 'red') ?
+        '../assets/image/icons/icon-torpedo-red.png' :
+        '../assets/image/icons/icon-torpedo-green.png',
+      style: 'width: 240px',
+      alt: ''
+    })
+  );
+}
 
-  socket.on('setTorpedoIcon', function (value) {
-    $('#icon-torpedo').html(
-      $('<img>', {
-        src: (value == 'red') ?
-          '../assets/image/icons/icon-torpedo-red.png' :
-          '../assets/image/icons/icon-torpedo-green.png',
-        style: 'width: 240px',
-        alt: ''
-      })
-    );
-  });
+function setDeg(value) {
+  $('#deg').html(value + '&deg;');
+}
 
-  socket.on('setDeg', function (value) {
-    $('#deg').html(value + '&deg;');
-  });
-
-  socket.on('setLong', function (value) {
-    $('#long').html(value + '<span class="digital-panel-unit"> m</span>');
-  });
-
-});
+function setLong(value) {
+  $('#long').html(value + '<span class="digital-panel-unit"> m</span>');
+}
